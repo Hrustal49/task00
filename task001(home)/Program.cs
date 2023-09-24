121b10133d61a8185fdc2814bdd8b10e2c506ec6
Hrustal49 <19polan@gmail.com>
2023-09-24 11:57:44 +0800

    создали новые файлы

---
+++ task001(home)/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2. напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число больше, а 
+// какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> int.MaxValue = -3
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32 (Console.Read());
+ 
+ Console.WriteLine ("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b);
+{
+    Console.WriteLine ("Большее число: + a + , меньшее число + b");
+}
+else
+{
+    Console.WriteLine ($"большее число, меньшее число");
+}
---
+++ task04/Program.cs
@@ -0,0 +1,23 @@
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до number
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.WriteLine("Enter an integer largr than 0: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count += 1;
+    }
+}
+
+else
+{
+    Console.WriteLine("Input error!");
+}
